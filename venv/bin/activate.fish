8bacd7ef5aa05b79caa247f9f2cd9c2eb6a94b46
Alex Newman <alexnewman@Alexs-16-MacBook-Pro.local>
2024-06-04 11:58:32 -0400

    Initial Commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/alexnewman/Scripts/zip-to-text/github-to-text-app/venv"
+set -gx VIRTUAL_ENV "/Users/alexnewman/Scripts/github-to-text-app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
